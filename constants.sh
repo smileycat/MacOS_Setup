d19440b92ce84479b8ab8b1c48805944ae055d25
smileycat <liaocrong@outlook.com>
2022-07-12 01:50:47 +1000

    Update script and apps

---
+++ constants.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BREW_FORMULAE="git node java zsh zsh-completions mas"
+# adobe-creative-cloud folx docker mongodb-compass tiles
+BREW_CASKS="visual-studio-code insomnia discord iina steam appcleaner musescore microsoft-word microsoft-excel"
+# QuickLook plugins
+QUICKLOOK="qlstephen qlmarkdown syntax-highlight"
+
+# Apps from App Store
+APPS=(
+    Messenger
+    Cascadea
+    "LINE"
+    "The Unarchiver"
+    "Intelligent Translator"
+    "Adblock Plus for Safari ABP"
+)
--- install.sh
+++ install.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
+# Please signin to App Store before executing the script
+
+source constants.sh
+echo "PROCEED TO INSTALL DEV TOOLS AND:"
+echo "QUICKLOOK  "$QUICKLOOK
+echo "FORMULAE   "$BREW_FORMULAE
+echo "CASKS      "$BREW_CASKS
+echo "APPS       "${APPS[@]}
+echo
+read -p "Continue? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
 echo "Installing xcode-cli tools..."
 xcode-select --install
 
+read -p "Once xcode installation is completed, press any key to continue..."
 echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing homebrew formulae..."
-brew install git node java zsh zsh-completions mas
+brew install $BREW_FORMULAE
 
 echo "Setting git configs..."
 git config --global user.name "smileycat"
 git config --global user.email "liaocrong@outlook.com"
 git config --global alias.st "status"
-git config --global alias.cm "commit -m"
-git config --global alias.acm "!git add . && git commit -m"
 git config --global credential.helper store
 
 echo "Installing ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Setting zsh configs..."
-cp ./settings/.vimrc ~/
-cp ./settings/.zshrc ~/
-cp ./settings/bullet-train.zsh-theme ~/.oh-my-zsh/themes/
+cp ./settings/.vimrc ~
+cp ./settings/.zshrc ~
+cp ./settings/bullet-train.zsh-theme ~/.oh-my-zsh/themes
 chsh -s $(which zsh)
 
 echo "Installing powerline-fonts..."
@@ -30,23 +40,18 @@ git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 rm -rf fonts
 
-# Please signin to App Store before starting the script
-apps=(
-    Messenger LINE unarchiver colorslurp JSONPeep Cascadea
-    "Intelligent Translator" "Adblock Plus for Safari ABP"
-)
-
-for app in "${apps[@]}"; do
+for app in "${APPS[@]}"; do
     echo -e "\nInstalling "$app"..."
     mas lucky "$app"
 done
 
 echo "Installing homebrew casks..."
-brew install --cask qlvideo qlstephen qlmarkdown syntax-highlight tiles
+brew install --cask $QUICKLOOK
+brew install --cask $BREW_CASKS
+
+echo "Restarting quicklook..."
 xattr -d -r com.apple.quarantine ~/Library/QuickLook/
-brew install --cask docker visual-studio-code google-chrome sourcetree intellij-idea-ce insomnia
-brew install --cask mongodb-compass iina steam folx appcleaner musescore adobe-creative-cloud
-brew install --cask microsoft-word microsoft-excel vmware-fusion
+qlmanage -r cache
 
 echo "Cleaning up homebrew..."
 brew cleanup
